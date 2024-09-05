e7a1d7a92f129dab74822712e1f6b051b1d9ad3d
grandeljay <github@grandels.email>
2024-09-05 14:58:18 +0200

    chore(module): update to v0.1.3

--- src/admin/includes/modules/system/grandeljay_cao_max_products.php
+++ src/admin/includes/modules/system/grandeljay_cao_max_products.php
@@ -13,7 +13,7 @@ use RobinTheHood\ModifiedStdModule\Classes\StdModule;
 
 class grandeljay_cao_max_products extends StdModule
 {
-    public const VERSION = '0.1.2';
+    public const VERSION = '0.1.3';
 
     public function __construct()
     {
